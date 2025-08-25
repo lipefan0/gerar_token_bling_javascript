15b7d14ddfb0b1454bfedad568afe956befd9068
Felipe Oliveira <lipefan0@gmail.com>
2025-08-25 14:34:00 -0300

    remove código comentado e atualiza a função generateToken para usar refresh_token

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ async function generateToken(client_id, client_secret, code) {
     "base64"
   ); // Transforma as credenciais em base64
 
+  // const response = await fetch("https://www.bling.com.br/Api/v3/oauth/token", {
+  //   method: "POST",
+  //   headers: {
+  //     Authorization: `Basic ${credentials}`,
+  //     "Content-Type": "application/x-www-form-urlencoded",
+  //     Accept: "application/json",
+  //   },
+  //   body: new URLSearchParams({
+  //     code: code,
+  //     grant_type: "authorization_code",
+  //   }),
+  // });
+
   const response = await fetch("https://www.bling.com.br/Api/v3/oauth/token", {
     method: "POST",
     headers: {
@@ -11,8 +24,8 @@ async function generateToken(client_id, client_secret, code) {
       Accept: "application/json",
     },
     body: new URLSearchParams({
-      code: code,
-      grant_type: "authorization_code",
+      refresh_token: "b44546a4b48e1c519ed443341b353df2c75587a2",
+      grant_type: "refresh_token",
     }),
   });
 
@@ -26,7 +39,8 @@ async function generateToken(client_id, client_secret, code) {
 }
 
 // Exemplo de uso
-const client_id = "";
-const client_secret = "";
-const code = "";
+const client_id = "e66960438ccb547a3c8ae1e2c266372ed843ff21";
+const client_secret =
+  "454fbb54c9b1a2e07014dd47b84b06064f03a00b99194586f4205bc932f0";
+const code = "a24e11f605e2aaf68b79c21d40665a387e3671d5";
 generateToken(client_id, client_secret, code);
